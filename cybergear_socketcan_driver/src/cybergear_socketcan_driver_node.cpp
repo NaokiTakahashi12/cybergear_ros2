2087325d4898b432b13753833539bf82aaee90d2
Naoki Takahashi <naoki.takahashi060@gmail.com>
2024-02-17 01:22:43 +0900

    lint

--- cybergear_socketcan_driver/src/cybergear_socketcan_driver_node.cpp
+++ cybergear_socketcan_driver/src/cybergear_socketcan_driver_node.cpp
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2024 Naoki Takahashi
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <cmath>
 #include <memory>
 #include <string>
@@ -190,7 +212,7 @@ void CybergearSocketCanDriverNode::subscribeCanFrameCallback(
   if (!m_cg_frame_id->isDevice(msg->id)) {
     return;
   }
-  // TODO m_params->wait_power_on
+  // TODO(Naoki Takahashi) m_params->wait_power_on
 
   if (m_cg_frame_id->isFault(msg->id)) {
     RCLCPP_ERROR(this->get_logger(), "Detect fault state from cybergear");
@@ -274,7 +296,7 @@ void CybergearSocketCanDriverNode::updateParameterTimerCallback()
   }
 }
 
-// TODO wait for result
+// TODO(Naoki Takahashi) wait for result
 void CybergearSocketCanDriverNode::enableTorqueServiceCallback(
   const std_srvs::srv::SetBool::Request::ConstSharedPtr & request,
   const std_srvs::srv::SetBool::Response::SharedPtr & response)
@@ -332,7 +354,7 @@ void CybergearSocketCanDriverNode::procFeedbackPacket(const can_msgs::msg::Frame
 
 void CybergearSocketCanDriverNode::setDefaultCanFrame(can_msgs::msg::Frame::UniquePtr & msg)
 {
-  // TODO m_params->wait_power_on
+  // TODO(Naoki Takahashi) m_params->wait_power_on
   if (!msg) {
     return;
   }
